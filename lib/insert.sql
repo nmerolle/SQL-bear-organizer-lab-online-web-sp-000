d1b6cd92a5055d670a9f6bc6e61d8c1d74b97118
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-03 00:29:01 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
-,
+
 (2,"Rowdy", 10, "M", "black", "intense", 1),
 (3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
 (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
